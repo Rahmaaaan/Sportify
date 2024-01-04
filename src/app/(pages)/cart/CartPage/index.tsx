0d7b83a1ef68c301430f12fe4710b05352fe0563
Aqueel ur Rahman Khan <119236505+Rahmaaaan@users.noreply.github.com>
2024-01-04 19:14:20 +0530

    delivery charge modal

--- src/app/(pages)/cart/CartPage/index.tsx
+++ src/app/(pages)/cart/CartPage/index.tsx
@@ -101,10 +101,10 @@ export const CartPage: React.FC<{
                   <h6 className={classes.cartTotal}>Summary</h6>
                 </div>
 
-                <div className={classes.row}>
+                {/* <div className={classes.row}>
                   <p className={classes.cartTotal}>Delivery Charge</p>
-                  <p className={classes.cartTotal}>$0</p>
-                </div>
+                  <p className={classes.cartTotal}>₹ 0</p>
+                </div> */}
 
                 <div className={classes.row}>
                   <p className={classes.cartTotal}>Grand Total</p>
